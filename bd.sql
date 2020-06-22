8074d968baed14df10147e409d9eaa765f2a8b5c
andersondln <59345168+andersondln@users.noreply.github.com>
2020-06-22 15:35:04 -0300

    Update bd.sql
    Criação da tabela "usuario".

--- bd.sql
+++ bd.sql
@@ -6,3 +6,15 @@ nome varchar(80) not null,
 telefone varchar(20) default null,
 email varchar(80) default null
 );
+
+-- Criação da Tabela Usuario
+
+CREATE TABLE usuario( 
+ usuario_id  INT(255) NOT NULL AUTO_INCREMENT, 
+ nome  VARCHAR(80) NOT NULL, 
+ email  VARCHAR(80) NOT NULL, 
+ senha  VARCHAR(255) NOT NULL, 
+ dt_criacao  DATE NOT NULL, 
+ dt_update  DATE, 
+ ativo  INT(2) NOT NULL, 
+PRIMARY KEY ( usuario_id ));
